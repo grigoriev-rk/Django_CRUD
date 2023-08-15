f604d55905fe0c1b631320cbbccc0a78a10dcc56
Roman Grigoriev <grigoriev.rk@gmail.com>
2023-08-15 19:45:52 +0300

    Initial commit

--- stocks_products/stocks_products/settings.py
+++ stocks_products/stocks_products/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'stocks_products',
     'logistic',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
