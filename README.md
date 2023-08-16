7bed67b43a067b0ae43a11b7607d563553d3caca
Roman Grigoriev <grigoriev.rk@gmail.com>
2023-08-16 20:32:57 +0300

    Initial commit

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 Работа должна соответствовать принятому [стилю оформления кода](https://github.com/netology-code/codestyle/tree/master/python).
 
 Любые вопросы по решению задач задавайте в чате учебной группы.
+![img.png](img.png)
+![img_1.png](img_1.png)
--- stocks_products/logistic/views.py
+++ stocks_products/logistic/views.py
@@ -3,18 +3,25 @@ from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework import generics
+from django_filters import rest_framework as filters
+
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
-    search_fields = ['title', 'description']
+    filter_backends = [filters.DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['title', 'description']
+
+
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
-    pagination_class = LimitOffsetPagination
+    filter_backends = [filters.DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['address', 'products', ]
+
+
+
